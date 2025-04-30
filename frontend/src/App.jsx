9861b7c78072d559b075b2e5cb2d3cf5674027e0
rafiahabib <rafiahabib46@gmail.com>
2025-04-30 18:00:17 +0500

    chang

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Navbar from './component/Navbar'
 import Homepage from './pages/Homepage'
@@ -10,7 +8,7 @@ function App() {
 
   return (
     <>
-    <Homepage/>
+     
       <Navbar/>
     </>
   )
